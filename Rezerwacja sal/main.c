f8db21ba7e78e3efb03472168e5b453ba0fd4aa3
10Nikus <kijasnikodem@gmail.com>
2025-04-14 17:59:44 +0200

    Dodano funkcje do otwierania i zapisywania plików

--- Rezerwacja sal/main.c
+++ Rezerwacja sal/main.c
@@ -1,4 +1,3 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,12 +5,23 @@
 
 #define MAX_LINE 1024
 
+void open_file(void);
+void write_file(void);
+
+
 int main() {
     setlocale(LC_ALL, "");
+    //write_file();
+    open_file();
+    return 0;
+}
+
+void open_file(void)
+{
     FILE* plik = fopen("rezerwacje.csv", "r");
     if (plik == NULL) {
         perror("Nie mo¿na otworzyæ pliku");
-        return 1;
+        return ;
     }
 
     char linia[MAX_LINE];
@@ -29,5 +39,18 @@ int main() {
     }
 
     fclose(plik);
-    return 0;
 }
+
+
+void write_file(void)
+{   
+    FILE* plik = fopen("rezerwacje.csv", "a");
+    if (plik == NULL) {
+        perror("Nie mo¿na otworzyæ pliku");
+        return ;
+    }
+    fprintf(plik, "6,Paulina,Maj,2025-04-23,10:00,12:00,102,Warsztaty\n");
+    printf("Dodano rezerwacje!\n");
+    fclose(plik);
+}
+
