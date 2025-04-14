3c51a3da5c12da38ea547cd4097b8fa5d4781d70
10Nikus <kijasnikodem@gmail.com>
2025-04-14 17:18:16 +0200

    Dodano obsługę pliku rezerwacje.csv w main.c

--- Rezerwacja sal/main.c
+++ Rezerwacja sal/main.c
@@ -1,7 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(void)
-{
-	printf("OK");
-	return 0;
+#define MAX_LINE 1024
+
+int main() {
+    FILE* plik = fopen("rezerwacje.csv", "r");
+    if (plik == NULL) {
+        perror("Nie mo¿na otworzyæ pliku");
+        return 1;
+    }
+
+    char linia[MAX_LINE];
+
+    while (fgets(linia, MAX_LINE, plik)) {
+        linia[strcspn(linia, "\n")] = 0;
+
+        char* context = NULL;
+        char* kolumna = strtok_s(linia, ",", &context);
+        while (kolumna != NULL) {
+            printf("%s\t", kolumna);
+            kolumna = strtok_s(NULL, ",", &context);
+        }
+        printf("\n");
+    }
+
+    fclose(plik);
+    return 0;
 }
