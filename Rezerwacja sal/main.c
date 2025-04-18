8bccef87d6b61f3d18ee91dc4468bd1378816de7
justynaoczko <justynaoczko6@gmail.com>
2025-04-18 16:07:35 +0200

    Aktualizacja wpisywania rezerwacji - dane pobierane od użytkownika

--- Rezerwacja sal/main.c
+++ Rezerwacja sal/main.c
@@ -1,9 +1,8 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <locale.h>
-
-#define _CRT_SECURE_NO_WARNINGS
 #define MAX_LINE 1024
 
 void open_file(void);
@@ -55,35 +54,54 @@ void write_file(void)
     char linia[MAX_LINE];
     int last_id = 0;
 
-        while (fgets(linia, MAX_LINE, plik)) {
-            linia[strcspn(linia, "\n")] = 0;
+    while (fgets(linia, MAX_LINE, plik)) {
+        linia[strcspn(linia, "\n")] = 0;
 
-            char* context = NULL;
-            char* kolumna = strtok_s(linia, ",", &context);
-            if (kolumna != NULL) {
-                int current_id = atoi(kolumna);
-                if (current_id > last_id) {
-                    last_id = current_id;
-                }
+        char* context = NULL;
+        char* kolumna = strtok_s(linia, ",", &context);
+        if (kolumna != NULL) {
+            int current_id = atoi(kolumna);
+            if (current_id > last_id) {
+                last_id = current_id;
             }
         }
+    }
 
     fclose(plik);
 
     int new_id = last_id + 1;
 
-    // Sprawdzenie czy nowe ID jest poprawne
-    printf("Nowe ID: %d\n", new_id);
+    char imie[50], nazwisko[50], data[15], godz_od[6], godz_do[6], sala[10], typ[50];
+
+    printf("Podaj imiê: ");
+    scanf("%49s", imie);
+
+    printf("Podaj nazwisko: ");
+    scanf("%49s", nazwisko);
+
+    printf("Podaj datê (RRRR-MM-DD): ");
+    scanf("%14s", data);
+
+    printf("Godzina rozpoczêcia (HH:MM): ");
+    scanf("%5s", godz_od);
+
+    printf("Godzina zakoñczenia (HH:MM): ");
+    scanf("%5s", godz_do);
+
+    printf("Sala: ");
+    scanf("%9s", sala);
+
+    printf("Typ wydarzenia: ");
+    scanf("%49s", typ);
 
     err = fopen_s(&plik, "rezerwacje.csv", "a");
     if (err != 0 || plik == NULL) {
         perror("Nie mo¿na otworzyæ pliku");
         return;
     }
- 
-    // Trzeba zmodyfikowaæ na pobieranie danych od u¿ytkownika - na razie wpisane na sztywno
-    fprintf(plik, "6,Barbara,Williams,2025-04-23,10:00,12:00,102,Warsztaty\n");
-    printf("Dodano rezerwacje!\n");
+
+    fprintf(plik, "%d,%s,%s,%s,%s,%s,%s,%s\n", new_id, imie, nazwisko, data, godz_od, godz_do, sala, typ);
+    printf("Dodano rezerwacjê z ID %d!\n", new_id);
 
     fclose(plik);
 }
