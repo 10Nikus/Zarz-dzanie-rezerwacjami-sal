750f09dde7e02d5c5942945053a052f94114e6d0
ppapiez@student.agh.edu.pl <ppapiez@student.agh.edu.pl>
2025-05-08 12:09:51 +0200

    .

--- Rezerwacja sal/main.c
+++ Rezerwacja sal/main.c
@@ -1,7 +1,50 @@
+#include "main.h"
 #include <stdio.h>
+#include <wchar.h>
 
-int main(void)
-{
-	printf("OK");
-	return 0;
+// Funkcja do przypisania brakuj¹cych ID istniej¹cym rezerwacjom
+void assign_ids_to_existing_reservations() {
+    // Kod funkcji pozostaje bez zmian
+}
+
+// Funkcja obs³uguj¹ca dialog dodawania rezerwacji
+INT_PTR CALLBACK AddReservationDialogProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam) {
+    // Kod funkcji pozostaje bez zmian
+}
+
+// Funkcja obs³uguj¹ca dialog usuwania rezerwacji
+INT_PTR CALLBACK RemoveReservationDialogProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam) {
+    // Kod funkcji pozostaje bez zmian
+}
+
+// Implementacja funkcji do zapisu do pliku
+void write_to_file(HWND hwnd) {
+    DialogBox(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_ADD_RESERVATION), hwnd, AddReservationDialogProc);
+}
+
+// Implementacja funkcji do odczytu z pliku
+void read_from_file(HWND hwnd) {
+    // Kod funkcji pozostaje bez zmian
+}
+
+// Funkcja wyszukiwania rezerwacji po ID
+void find_reservation_by_id(HWND hwnd) {
+    // Kod funkcji pozostaje bez zmian
+}
+
+// Funkcja do przypisania brakuj¹cych ID
+void initialize_reservations() {
+    assign_ids_to_existing_reservations();
+}
+
+// Funkcja do usuwania rezerwacji
+void remove_from_file(HWND hwnd) {
+    DialogBox(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_REMOVE_RESERVATION), hwnd, RemoveReservationDialogProc);
+}
+
+// G³ówna funkcja programu
+int main() {
+    initialize_reservations();
+    // Reszta kodu programu
+    return 0;
 }
